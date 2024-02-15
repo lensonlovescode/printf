a5b4d36d64b7b3a2fdca94870bed25c6d70399a9
Sharon Wainaina <navion8759@gmail.com>
2024-02-15 16:17:27 +0300

    added last four function definitions

--- functions.c
+++ functions.c
@@ -8,8 +8,7 @@ int _putchar(char ch)
 }
 /**
  * Prints a single character to the standard output.
- *
- * @param ch The character to be printed.
+ *@param ch The character to be printed.
  * @return The number of characters printed (always 1).
  */
 int print_char(va_list args)
@@ -49,33 +48,134 @@ int print_percent(va_list args __attribute__((unused)))
 	return (1);
 }
 /**
- * print_int - prints an integer
- * @num: the integer to be printed
- * Return: number of characters printed
+ * print_int - Prints an integer to the standard output.
+ * @param args The variable argument list containing the integer to be printed.
+ * @return The number of characters printed successfully, or -1 on error.
  */
-/**int print_int(const char *num)
+int print_int(va_list args) 
 {
-	int count;
-
-	count = 0;
-	int value = atoi(num);
-	//use putchar to print the integer then increment cunt
-	return (count);
+    int num = va_arg(args, int);
+    int is_negative = 0; 
+    if (num < 0) 
+	{
+        num = -num;
+        is_negative = 1;
+    }
+	int count = 0; 
+    int digits[20]; 
+    int digit_index = 0;
+	while (num > 0) 
+	{
+        if (digit_index >= sizeof(digits) / sizeof(digits[0])) 
+		{
+            return -1;
+        }
+        digits[digit_index++] = num % 10;
+        num /= 10;
+    }
+    if (is_negative) 
+	{
+        _putchar('-');
+        count++;
+    }
+    for (int i = digit_index - 1; i >= 0; i--) 
+	{
+        _putchar(digits[i] + '0');
+        count++;
+    }
+	return count;
 }
-*/
 /**
- * print_int_two - prints an integer
- * @num: the integer to be printed
- * Return: number of characters printed
+ * print_int_two - Prints an integer to the standard output.
+ * @param args The variable argument list containing the integer to be printed.
+ * @return The number of characters printed successfully, or -1 on error.
  */
-/*int print_int_two(const char *num)
+int print_int_two(va_list args) 
 {
-	int count;
+    int num = va_arg(args, int);
+    int is_negative = 0; 
+	if (num < 0)
+	{
+        num = -num;
+        is_negative = 1;
+    }
+	int count = 0; 
+    int digits[20]; 
+    int digit_index = 0;
+	while (num > 0) 
+	{
+        if (digit_index >= sizeof(digits) / sizeof(digits[0])) 
+		{
+            return -1;
+        }
+        digits[digit_index++] = num % 10;
+        num /= 10;
+    }
+    if (is_negative) 
+	{
+        _putchar('-');
+        count++;
+    }
+    for (int i = digit_index - 1; i >= 0; i--) 
+	{
+        _putchar(digits[i] + '0');
+        count++;
+    }
+	return count;
+}
+/**
+ * print_uint-Prints an unsigned integer to the standard output in decimal format.
+ * @param args The variable argument list containing the unsigned integer to be printed.
+ * @return The number of characters printed successfully, or -1 on error.
+ */
+int print_uint(va_list args)
+ {
+    unsigned int num = va_arg(args, unsigned int);
+    int count = 0;
+    int digits[20]; 
+    int digit_index = 0;
 
-	count = 0;
-	int value = atoi(num);
-	//use putchar to print integer then increment count
-	return (count);
+    while (num > 0) 
+	{
+        if (digit_index >= sizeof(digits) / sizeof(digits[0])) 
+		{
+            return -1; 
+        }
+        digits[digit_index++] = num % 10;
+        num /= 10;
+    }
+    for (int i = digit_index - 1; i >= 0; i--)
+	 {
+        _putchar(digits[i] + '0');
+        count++;
+    }
+	return count;
+}
+/**
+ * Print_octal-Prints an unsigned integer to the standard output in octal format.
+ * @param args The variable argument list containing the unsigned integer to be printed.
+ * @return The number of characters printed successfully, or -1 on error.
+ */
+int print_octal(va_list args) 
+{
+    unsigned int num = va_arg(args, unsigned int);
+    int count = 0;
+    int digits[20]; 
+    int digit_index = 0;
 
-
-}*/
+    while (num > 0) 
+	{
+         if (digit_index >= sizeof(digits) / sizeof(digits[0])) 
+		{
+            return -1; 
+        }
+        digits[digit_index++] = num % 8; 
+        num /= 8;
+    }
+    for (int i = digit_index - 1; i >= 0; i--) 
+	{
+        _putchar(digits[i] + '0');
+        count++;
+    }
+	return count;
+}
