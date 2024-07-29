f3bb8f57eafa0d20e404a273fb78cc063aafc9c6
lensonlovescode <lensonisinparis@gmail.com>
2024-07-29 17:40:58 +0300

    null string

--- functions_2.c
+++ functions_2.c
@@ -2,7 +2,6 @@
 /**
  * print_hexa - Prints the hexadecimal in lowercase.
  * @args: The va_list containing the unsigned int to be printed.
- *
  * Return: The number of characters printed.
  */
 int print_hexa(va_list args)
@@ -41,7 +40,6 @@ int print_hexa(va_list args)
 /**
  * print_hexa_upper - Prints the hexadecimal in uppercase.
  * @args: The va_list containing the unsigned int to be printed.
- *
  * Return: The number of characters printed.
  */
 int print_hexa_upper(va_list args)
@@ -83,7 +81,6 @@ int print_hexa_upper(va_list args)
 /**
  * get_significant_digits - Get the number of significant digits
  * @num: The number to check
- *
  * Return: The number of significant digits
  */
 int get_significant_digits(unsigned int num)
@@ -104,12 +101,17 @@ int get_significant_digits(unsigned int num)
  */
 int print_int(va_list args)
 {
-	int num = va_arg(args, int);
-	int is_negative = 0;
-	int count = 0;
+	int num;
+	int is_negative;
+	int count;
 	int digits[20] = {0};
-	unsigned int digit_index = 0;
+	unsigned int digit_index;
 	int i;
+
+	num = va_arg(args, int);
+	is_negative = 0;
+	count = 0;
+	digit_index = 0;
 
 	if (num == 0)
 	{
@@ -143,18 +145,23 @@ int print_int(va_list args)
 	return (count);
 }
 /**
- * print_int_two - Prints an integer to the standard output.
+ * print_int_two - Prints a long integer to the standard output.
  * @args: The variable argument list containing the integer to be printed.
  * Return: The number of characters printed successfully, or -1 on error.
  */
 int print_int_two(va_list args)
 {
-	long int num = va_arg(args, int);
-	int is_negative = 0;
-	int count = 0;
+	long int num;
+	int is_negative;
+	int count;
 	int digits[20];
-	unsigned int digit_index = 0;
+	unsigned int digit_index;
 	int i;
+
+	num = va_arg(args, int);
+	is_negative = 0;
+	count = 0;
+	digit_index = 0;
 
 	if (num == 0)
 	{
